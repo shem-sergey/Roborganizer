a32665da8e890f3b6536ac8173febf0e2c46bebf
shem-sergey <sergey.shemyakov@duallab.com>
2016-08-31 17:04:47 +0300

    Added shell scripts for installing and running.

---
+++ install.sh
@@ -0,0 +1,2 @@
+mkdir target
+javac -cp . -d target -sourcepath src src/roborganizer/*.java src/roborganizer/datePatterns/*.java src/roborganizer/cli/*.java
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp target roborganizer.cli.Roborganizer
